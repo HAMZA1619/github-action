0985e6fe50f0b2ec9c923074f949a8b7dbdd14e0
hamza elglaoui <hamza.elglaoui00@gmail.com>
2025-10-15 15:36:44 +0100

    see if it will work

--- puppeteer-script.js
+++ puppeteer-script.js
@@ -8,8 +8,10 @@ const path = require('path');
     args: ['--no-sandbox', '--disable-setuid-sandbox']
   });
   const page = await browser.newPage();
+    page.setViewport({ width: 1280, height: 851 });
+    const url = websites[Math.floor(Math.random() * websites.length)];
 
-  await page.goto('https://www.lightfunnels.com/', { waitUntil: 'networkidle2' });
+  await page.goto(url, { waitUntil: 'networkidle2' });
 
   // Create screenshots folder
   const dir = path.join(__dirname, 'public/screenshots');
@@ -22,3 +24,37 @@ const path = require('path');
 
   await browser.close();
 })();
+
+const websites = [
+  "https://www.google.com",
+  "https://www.apple.com",
+  "https://www.microsoft.com",
+  "https://www.amazon.com",
+  "https://www.netflix.com",
+  "https://www.spotify.com",
+  "https://www.youtube.com",
+  "https://www.wikipedia.org",
+  "https://www.nasa.gov",
+  "https://www.tesla.com",
+  "https://www.bbc.com",
+  "https://www.cnn.com",
+  "https://www.nytimes.com",
+  "https://www.theverge.com",
+  "https://techcrunch.com",
+  "https://www.adobe.com",
+  "https://www.paypal.com",
+  "https://www.cloudflare.com",
+  "https://www.openai.com",
+  "https://vercel.com",
+  "https://www.airbnb.com",
+  "https://www.shopify.com",
+  "https://www.x.com",            // Twitter
+  "https://www.instagram.com",
+  "https://www.linkedin.com",
+  "https://www.reddit.com",
+  "https://www.github.com",
+  "https://developer.mozilla.org",
+  "https://www.stackoverflow.com",
+  "https://www.producthunt.com",
+  "https://dribbble.com"
+];
